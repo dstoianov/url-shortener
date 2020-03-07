738eb070698c207cf9e5a31aa064745e9f12a000
Denys Stoianov <denys.stoianov@raisin.com>
2020-03-07 13:47:50 +0100

    added more tests for controller

--- src/main/java/se/techinsight/urlshortener/api/dto/OriginalUrlDto.java
+++ src/main/java/se/techinsight/urlshortener/api/dto/OriginalUrlDto.java
@@ -1,12 +1,16 @@
 package se.techinsight.urlshortener.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.Size;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OriginalUrlDto {
 
     @URL(message = "please, provide valid URL")
