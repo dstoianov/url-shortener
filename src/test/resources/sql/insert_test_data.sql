ef93ee64b8c85ce5e9cb5c42207de2ee10009129
Denys Stoianov <denys.stoianov@raisin.com>
2020-03-09 21:27:26 +0100

    tided data in db with ids

--- src/test/resources/sql/insert_test_data.sql
+++ src/test/resources/sql/insert_test_data.sql
@@ -1,12 +1,6 @@
 -- URL
-
 INSERT INTO url_shortener (id, long_url, shorten_key)
-VALUES (1, 'https://www.google.com', 'g0');
-INSERT INTO url_shortener (long_url, shorten_key)
-VALUES ('https://www.google.com', 'g1');
-INSERT INTO url_shortener (long_url, shorten_key)
-VALUES ('https://www.google.com.ua', 'g2');
-INSERT INTO url_shortener (long_url, shorten_key)
-VALUES ('https://www.google.de', 'g3');
-INSERT INTO url_shortener (long_url, shorten_key)
-VALUES ('http://g.zeos.in/?q=spring%20boot%20redirect', 'g4');
+VALUES (1, 'https://www.google.com', 'g1'),
+       (2, 'https://www.google.com.ua', 'g2'),
+       (3, 'https://www.google.de', 'g3'),
+       (4, 'http://g.zeos.in/?q=spring%20boot%20redirect', 'g4');
